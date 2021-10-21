585036bcd65c49d06c4f9806c6c6d5afab5f81a7
Аведик Млтыхян <aviedik.mltykhian@mail.ru>
2021-10-21 09:10:23 +0300

    Add function LaterInYear

--- DateTime.pas
+++ DateTime.pas
@@ -3,6 +3,11 @@ begin
   Result := if (DateTime.Now.Year mod 4 = 0) and ((DateTime.Now.Year mod 100 <> 0) or (DateTime.Now.Year mod 400 = 0));
 end;
 
+function LaterInYear(DateTime day1, DateTime day2): DateTime;
+begin
+  Result := if DateTime.Compare(day1, day2) < 0 then day1 else day2;
+end;
+
 begin
   
 end.
