076cfbe9ac02af7ff561062355186127637280f6
Аведик Млтыхян <aviedik.mltykhian@mail.ru>
2021-10-21 08:54:46 +0300

    Add function IsLeapYear

---
+++ DateTime.pas
@@ -0,0 +1,8 @@
+﻿function IsLeapYear(DateTime date): boolean;
+begin
+  Result := if (DateTime.Now.Year mod 4 = 0) and ((DateTime.Now.Year mod 100 <> 0) or (DateTime.Now.Year mod 400 = 0));
+end;
+
+begin
+  
+end.
