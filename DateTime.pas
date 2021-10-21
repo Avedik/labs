7feabda4342f893f8424f0849d809f10ae33c64d
Аведик Млтыхян <aviedik.mltykhian@mail.ru>
2021-10-21 08:57:14 +0300

    Revert "Add function SecondsInMinute"
    This reverts commit 174e626ebee9daa06b5948182c066510953dd4dc.

--- DateTime.pas
+++ DateTime.pas
@@ -3,8 +3,6 @@ begin
   Result := if (DateTime.Now.Year mod 4 = 0) and ((DateTime.Now.Year mod 100 <> 0) or (DateTime.Now.Year mod 400 = 0));
 end;
 
-function SecondsInMinute() := 60;
-
 begin
   
 end.
